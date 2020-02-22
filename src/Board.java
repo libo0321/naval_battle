a6c81bc390023bdf4f6c5c6a00688dbffbc8e5e9
libo0321 <basile.lee@qq.com>
2020-02-22 02:36:22 +0100

    Exercice 3

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import Ship.*;
 
-public class Board{
+public class Board implements IBoard{
     private String name;
     private char[][] navires;
     private boolean[][] frappes;
@@ -25,40 +29,6 @@ public class Board{
         this(name, 10, 10);
     }
 
-    public void print(){
-        char begin_letter='A';
-        System.out.println("Navires : " );
-        System.out.print("  ");
-        for (int i=(int)begin_letter; i<(int)begin_letter+length; i++){
-            System.out.print(" "+(char)i);
-        }
-        System.out.print("\n");
-        for (int j = 1; j<=width; j++){
-            System.out.print(j);
-            System.out.print(" ");
-            for (int i = 0; i < length; i++){
-                System.out.print(". ");
-            }
-            System.out.print("\n");
-        }
-        System.out.println("Frappes : " );
-        System.out.print("  ");
-        for (int i=(int)begin_letter; i<(int)begin_letter+length; i++){
-            System.out.print(" "+(char)i);
-        }
-        System.out.print("\n");
-        for (int j = 1; j<=width; j++){
-            System.out.print(j);
-            System.out.print(" ");
-            for (int i = 0; i < length; i++){
-                if (frappes[i][j-1])
-                    System.out.print("x ");
-                else System.out.print(". ");
-            }
-            System.out.print("\n");
-        }
-    }
-
     public String getName() {
         return name;
     }
@@ -74,5 +44,135 @@ public class Board{
     public void setMName(String name) {
         this.name = name;
     }
+
+    @Override
+    public int getSize() {
+        return width * length;
+    }
+
+    @Override
+    public boolean hasShip(int x, int y){
+        if (navires[x][y] == 'S') return true;
+        else return false;
+    }
+
+    public void setHit(boolean hit, int x, int y){
+        if(hit) frappes[x][y] = true;
+        else frappes[x][y] = false;
+    }
+
+    public Boolean getHit(int x, int y){
+        if(hasShip(x,y)) return true;
+        else return false;
+    }
+
+    @Override
+    public void putShip(AbstractShip ship, int x, int y){
+        if (!(x<0 || y<0 || x>=length || y>=width)) {
+            boolean no_ship = true;
+            int i = 0;
+            switch (ship.getOrientation()) {
+                case EAST:
+                    if (x - ship.getTaille() >= -1) {
+                        //test if a ship has occupied the place
+                        while (i < ship.getTaille()&&no_ship){
+                            if (navires[x-i][y] == 'S') {no_ship = false;}
+                            i++;
+                        }
+                        //if there is no ship
+                        if (no_ship)
+                            for (int j = 0; j<ship.getTaille(); j++)
+                                navires[x-j][y] = 'S';
+                    }
+                    break;
+
+                case WEST:
+                    if (x + ship.getTaille() <= length) {
+                        //test if a ship has occupied the place
+                        while (i < ship.getTaille()&&no_ship){
+                            if (navires[x+i][y] == 'S') {no_ship = false;}
+                            i++;
+                        }
+                        //if there is no ship
+                        if (no_ship)
+                            for (int j = 0; j<ship.getTaille(); j++)
+                                navires[x+j][y] = 'S';
+                    }
+                    break;
+
+                case NORTH:
+                    if (y + ship.getTaille() <= width) {
+                        //test if a ship has occupied the place
+                        while (i < ship.getTaille()&&no_ship){
+                            if (navires[x][y+i] == 'S') {no_ship = false;}
+                            i++;
+                        }
+                        //if there is no ship
+                        if (no_ship)
+                            for (int j = 0; j<ship.getTaille(); j++)
+                                navires[x][y+j] = 'S';
+                    }
+                    break;
+
+                case SOUTH:
+                    if (y - ship.getTaille() >= -1) {
+                        //test if a ship has occupied the place
+                        while (i < ship.getTaille()&&no_ship){
+                            if (navires[x][y-i] == 'S') {no_ship = false;}
+                            i++;
+                        }
+                        //if there is no ship
+                        if (no_ship)
+                            for (int j = 0; j<ship.getTaille(); j++)
+                                navires[x][y-j] = 'S';
+                    }
+                    break;
+            }
+        }
+    }
+
+    /**
+     * print the table Navires and Frappes
+     */
+    public void print(){
+        char begin_letter='A';
+        System.out.println("Navires : " );
+        System.out.print("  ");
+        for (int i=(int)begin_letter; i<(int)begin_letter+length; i++){
+            System.out.print(" "+(char)i);
+        }
+        System.out.print("\n");
+        for (int j = 1; j<=width; j++){
+            System.out.print(j);
+
+            if (j<10) System.out.print("  "); //make sure the table is aligned
+            else System.out.print(" ");
+
+            for (int i = 0; i < length; i++){
+                System.out.print(navires[i][j-1]+" ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("Frappes : " );
+        System.out.print("  ");
+        for (int i=(int)begin_letter; i<(int)begin_letter+length; i++){
+            System.out.print(" "+(char)i);
+        }
+        System.out.print("\n");
+        for (int j = 1; j<=width; j++){
+            System.out.print(j);
+
+            if (j<10) System.out.print("  "); //make sure the table is aligned
+            else System.out.print(" ");
+
+            for (int i = 0; i < length; i++){
+                if (frappes[i][j-1])
+                    System.out.print("x ");
+                else System.out.print(". ");
+            }
+            System.out.print("\n");
+        }
+    }
+
 
 }
--- src/TestBoard.java
+++ src/TestBoard.java
@@ -1,11 +1,18 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 import Ship.*;
 
 public class TestBoard{
     public static void main(String [] args) {
         Board board = new Board("Test",11,13);
+        Submarine s = new Submarine("ji",4,Orientation.EAST);
+        Carrier c = new Carrier("ca",5, Orientation.NORTH);
+        Carrier d = new Carrier("ca",5, Orientation.SOUTH);
+        board.putShip(s,5,6);
+        board.putShip(c,5,5);
+        board.putShip(d,5,5);
         board.print();
 
     }
